9844e4942cf8f325f82057d9436460beea6167e3
Dr David Martin <d.m.a.martin@dundee.ac.uk>
2012-12-07 11:39:21 +0000

    added neutral loss parsing and convenience methods for db.get_delta_mono(label) and db.get_neutral_loss(label, aa)

--- PyUniMod/setup.py
+++ PyUniMod/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name='Unimod',
-    version="0.1.0",
+    version="0.1.1",
     description="simple wrapper around the Unimod amino acid modificatins database",
     packages=find_packages(),
     install_requires=[''],
